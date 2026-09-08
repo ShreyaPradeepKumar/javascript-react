f4e95c50dfbed1dd149b1790e27b5e22c46b7154
ShreyaPradeepKumar <shreyapradeepkumar74@gmail.com>
2026-09-08 14:57:53 +0530

    Day 2 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
-function App(){
-  return <h1>Hello, World I'm learning react</h1>
+export default function App() {
+  return (
+    <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+      <Header />
+    </div>
+  );
 }
-export default App
+
+function Header() {
+  return (
+    <header className="flex- items-center justify-between ">
+     <div className="flex flex-col gap-1">
+      <h1 className="text-3xl font-bold">Habit Tracker</h1>
+      <span className="text-zinc-400 text-sm">1/1 done today</span>
+     </div>
+     <div className="flex flex-col gap-1">
+      <span className="text-zinc-400 text-sm"> Sept 8 to Oct 8</span>
+     </div>
+    </header>
+  );
+}
