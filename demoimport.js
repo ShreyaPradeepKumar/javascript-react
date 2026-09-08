d7670dade6225e21c3b9e0c13e08800c4879046d
ShreyaPradeepKumar <shreyapradeepkumar74@gmail.com>
2026-09-08 11:36:14 +0530

    Restore Day 1 files

--- demoimport.js
+++ demoimport.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import { capitalizeString }  from "./day1.js"
 const cap=capitalizeString("hello!");
 console.log(cap);
-=======
-import { capitalizeString }  from "./day1.js"
-const cap=capitalizeString("hello!");
-console.log(cap);
->>>>>>> 61421b1d9b7e351506f20280c5bd023ad2aa3ef1
