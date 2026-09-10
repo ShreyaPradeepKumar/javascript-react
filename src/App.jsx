cbbc9bb85649ac40ead638ae61e4f22f9c6fe0ec
ShreyaPradeepKumar <shreyapradeepkumar74@gmail.com>
2026-09-10 13:58:20 +0530

    Day 3 single page router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
 import './App.css'
 import { useState } from 'react';
 
-function Greeting(prop){
-  return <h2>Hello World {prop.name} you are {prop.age} years old</h2>
+function HomePage(){
+  return <h1>Welcome to the Home Page</h1>
 }
-
 function App() {
-  const [showGreeting, setShowGreeting] = useState(false);
-
-
-  const name = getName();
-  const age = 22; 
-
-  function greet(){
-    if(showGreeting){    
-      setShowGreeting(false);
-    } else {
-      setShowGreeting(true);
-    }
-  }
-
-  return (
-   <div>
-    <button onClick={greet}>clickme</button>
-    {showGreeting && <Greeting name={name} age={age}/>}
-   </div>
-  )
+  return
+   <div><Routes> 
+    <Route path="/" element={<HomePage />} />
+  </Routes>
+</div>
 }
-
-function getName(){
-  return "Shreya";
-}
-
 export default App;
