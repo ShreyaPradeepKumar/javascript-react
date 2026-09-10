0766beecbbada6c7a1bf7c47d98ba5070272846e
ShreyaPradeepKumar <shreyapradeepkumar74@gmail.com>
2026-09-10 14:04:45 +0530

    Day 3 final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import { useState } from 'react';
 
-function HomePage(){
-  return <h1>Welcome to the Home Page</h1>
+function AboutPage() {
+  return <div>About Page</div>;
 }
+
 function App() {
-  return
-   <div><Routes> 
-    <Route path="/" element={<HomePage />} />
-  </Routes>
-</div>
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>  
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+      <div>Footer</div>
+    </div>
+  );
 }
+
 export default App;
